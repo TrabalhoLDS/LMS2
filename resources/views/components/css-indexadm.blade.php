c309b42a2c4b40c44cf091f6387adef243f94f49
ddaniks <101910685+ddaniks@users.noreply.github.com>
2024-04-11 20:49:15 -0300

    Create css-indexadm.blade.php
    component do css view adm

---
+++ resources/views/components/css-indexadm.blade.php
@@ -0,0 +1,65 @@
+<style>
+        .custom-search-form {
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+@media (max-width: 576px) {
+  .custom-search-form {
+      max-width: 100%;
+      padding: 0 15px;
+  }
+}
+
+.full-width-line {
+  margin: 20px -15px;
+  border-top: 1px solid #dee2e6;
+}
+
+.inner-line {
+  margin: 10px 0;
+  border-top: 1px solid #dee2e6;
+}
+
+.p {
+  color: black;
+}
+
+/* Estilização para as imagens */
+.same-size-img {
+  height: 40px; /* Definindo a altura desejada */
+  width: 40px;  /* Definindo a largura desejada */
+  object-fit: cover; /* Garantindo que a imagem cubra todo o espaço */
+}
+
+.centered-names {
+  display: flex;
+  align-items: center;  /* Aligns items vertically */
+  justify-content: space-between;
+}
+/* Estilização para o botão dropdown */
+.dropdown-toggle {
+  font-size: 0.8rem;
+  padding: 0.2rem 0.5rem;
+  margin-top: 0; /* Removendo o espaçamento superior */
+}
+
+/* Espaçamento entre o botão e o nome */
+.btn-name-spacing {
+  margin-right: 10px;
+}
+
+.icones {
+  float: right; /* Alinha os ícones à direita */
+}
+
+.icones i {
+  margin-left: 10px; /* Espaçamento entre os ícones */
+  color: #666; /* Cor dos ícones */
+  cursor: pointer; /* Cursor de mão ao passar por cima */
+}
+
+.icones i:hover {
+  color: #333; /* Mudança de cor ao passar o mouse */
+}
+    </style>
