f5782e9135cc1a755b3c24647809d96aeb92d141
Lemeson <lemeson93@gmail.com>
2024-04-17 20:25:26 -0300

    Start de Controller Model teste migration

---
+++ app/Models/Profsessor.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profsessor extends Model
+{
+    use HasFactory;
+}
