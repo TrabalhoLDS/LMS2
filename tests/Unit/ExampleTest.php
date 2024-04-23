5af8c3a67a46f876c635872632f8fc5095299264
ddaniks <danix367@gmail.com>
2024-04-22 23:13:43 -0300

    testes+comentarios

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,4 +13,42 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function testExemploRota()
+    {
+        $response = $this->get('/exemplo-rota');
+
+        $response->assertStatus(200);
+    }
+
+    public function testExemplohome()
+    {
+        $response = $this->get('/materiasProf');
+      
+        $response->assertStatus(200);
+    } 
+
+    /*
+        com este comando "php artisan test tests/Feature/ExemploTeste.php"
+        é possivel realisar texte diretamente no arquivo criado como "ExempleTest.php"
+
+        para realisar texte em todo diretorio texte é usado "php artisan test"
+
+        e para realisar textes apenas em uma determinada classe como no "ExampleTesr.php" é usado "php artisan test --filter ExemploTeste
+    */
+
+    /*   
+        *************RESULTADO***************
+
+        php artisan test tests/Feature/ExampleTest.php
+
+        PASS  Tests\Feature\ExampleTest
+        ✓ the application returns a successful response                                                                                                                     0.07s  
+
+        Tests:    1 passed (1 assertions)
+        Duration: 0.14s
+
+    */
+              
+    
 }
