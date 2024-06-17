a5fc5cd2d67697e972a506d2a1656737e0ee276d
Lemeson <lemeson93@gmail.com>
2024-06-16 22:12:26 -0300

    relacionamento turma faltando configurar os as tabelas intermediarias 'tabelas pivo'

---
+++ app/Models/Turma.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Turma extends Model
+{
+    use HasFactory;
+
+    // Define a relação com usuários como professores
+    public function professores()
+    {
+        return $this->belongsToMany(User::class, 'professor_turma', 'turma_id', 'user_id');
+    }
+
+    // Define a relação com usuários como alunos
+    public function alunos()
+    {
+        return $this->belongsToMany(User::class, 'aluno_turma', 'turma_id', 'user_id');
+    }
+}
