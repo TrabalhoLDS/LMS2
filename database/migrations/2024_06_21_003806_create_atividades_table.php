3948d17fcb3136e9fca779011dee6abe39e22ebd
Lemeson <lemeson93@gmail.com>
2024-06-20 22:08:57 -0300

    Criação das tabelas atividade e aula

---
+++ database/migrations/2024_06_21_003806_create_atividades_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('atividades', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('descricao');
+            $table->date('dataAbertura');
+            $table->date('dataExpiracao');
+            $table->string('tipoAtividade');
+            $table->string('caminhoArquivo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('atividades');
+    }
+};
