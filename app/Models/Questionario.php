58c67ef6e986a87d30e1ef2731474d94ce62832a
Lemeson <lemeson93@gmail.com>
2024-08-12 19:41:55 -0300

    questionario esqueleton

---
+++ app/Models/Questionario.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Questionario extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['titulo', 'descricao'];
+
+    public function questoes()
+    {
+        return $this->hasMany(Questao::class);
+    }
+}
